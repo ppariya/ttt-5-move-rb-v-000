1a24be1fd2ba1b01d199462e04419a5c916e2194
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-01-09 22:51:42 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(input)
   move = input.to_i - 1
 end
 
-def move(board, move, PlayerChar)
+def move(board, move)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   def update_array_at_with(array, index, value)
   array[index] = value
